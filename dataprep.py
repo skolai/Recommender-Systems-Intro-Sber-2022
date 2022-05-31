a1d611e9a30c3203e1c9d93d1e29ba77e895c554
Evgeny Frolov <evfro@live.ru>
2022-05-31 10:30:36 +0300

    add MF examples

--- dataprep.py
+++ dataprep.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
 def transform_indices(data, users, items):
     data_index = {}
     for entity, field in zip(['users', 'items'], [users, items]):
@@ -11,3 +14,21 @@ def to_numeric_id(data, field):
     idx = idx_data.cat.codes
     idx_map = idx_data.cat.categories.rename(field)
     return idx, idx_map
+
+
+def matrix_from_data(data, data_description, dtype=None):
+    '''
+    Converts pandas DataFrame into sparse CSR matrix.
+    Assumes data in the DataFrame is alread normalized via `transform_indices`.
+    '''
+    # get indices of observed data
+    user_idx = data[data_description['users']].values
+    item_idx = data[data_description['items']].values
+    feedback_data = data_description.get('feedback', None)
+    if feedback_data is not None:
+        feedback = data[feedback_data].values
+    else:
+        feedback = np.ones(len(user_idx))
+    # construct rating matrix
+    shape = (data_description['n_users'], data_description['n_items'])
+    return csr_matrix((feedback, (user_idx, item_idx)), shape=shape, dtype=dtype)
